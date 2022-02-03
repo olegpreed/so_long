2fadde8aa21c595ed7589dea5d8d5dfdf77862b8
Олег <oleg@Air-Ksenia-2.rinet.ru>
2022-02-03 15:34:57 +0300

    still bad animation

--- game_over.c
+++ game_over.c
@@ -6,30 +6,32 @@
 /*   By: oleg <oleg@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 17:02:44 by oleg              #+#    #+#             */
-/*   Updated: 2022/02/02 23:03:35 by oleg             ###   ########.fr       */
+/*   Updated: 2022/02/03 15:34:19 by oleg             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-// void	slow_image(t_root *game, int x, int y)
-// {
-// 	int	c;
+void	slow_image(t_root *game, int x, int y)
+{
+	int	c;
 
-// 	c = 0;
-// 	while (c < 100)
-// 	{
-// 		//mlx_put_image_to_window(game->mlx, game->mlxw, game->i.max_o.reference, x, y);
-// 		c++;
-// 	}
-// }
+	c = 0;
+	while (c < 100)
+	{
+		mlx_put_image_to_window(game->mlx, game->mlxw, game->i.max_o.reference, x, y);
+		c++;
+	}
+}
 
 void	max_kicked_out(t_root *game)
 {
+	static int	x;
+	static int	y;
 	static int	c;
 	static int	k;
 
-	if (c < 70 && !k)
+	if (c < 100 && !k)
 	{
 		c++;
 		return ;
@@ -39,33 +41,30 @@ void	max_kicked_out(t_root *game)
 		game->i.max_o.symbol_index = 42;
 		game->i.max_o.pixel_loc.y++;
 		game->i.max_o.pixel_loc.x++;
-		// game->i.max_o.pixel_loc.y = game->pixelsize.y - game->i.wall.size.y;
-		// game->i.max_o.pixel_loc.x = game->pixelsize.x - 4 * game->i.wall.size.x + 10;
-		// k = 1;
-		// while (game->i.max_o.pixel_loc.x > game->pixelsize.x - 4 * game->i.wall.size.x - 100)
-		// {
-		// 	while (game->i.max_o.pixel_loc.y  > game->pixelsize.y - game->i.wall.size.y - 30)
-		// 	{
-		// 		//slow_image(game, x, y);
-		// 		game->i.max_o.pixel_loc.y--;
-		// 		game->i.max_o.pixel_loc.x--;
-		// 	}
-		// 	while (game->i.max_o.pixel_loc.y < game->pixelsize.y - game->i.wall.size.y)
-		// 	{
-		// 		//slow_image(game, x, y);
-		// 		game->i.max_o.pixel_loc.y++;
-		// 		game->i.max_o.pixel_loc.x--;
-		// 	}
-		// }
+		y = game->pixelsize.y - game->i.wall.size.y;
+		x = game->pixelsize.x - 4 * game->i.wall.size.x + 10;
+		k = 1;
+		while (x > game->pixelsize.x - 4 * game->i.wall.size.x - 100)
+		{
+			while (y > game->pixelsize.y - game->i.wall.size.y - 30)
+			{
+				slow_image(game, x, y);
+				y--;
+				x--;
+			}
+			while (y < game->pixelsize.y - game->i.wall.size.y)
+			{
+				slow_image(game, x, y);
+				y++;
+				x--;
+			}
+		}
 	}
 }
 
 void	game_over(t_root *game)
 {
-	game->over = 1;
 	fade_to_black(game);
 	max_kicked_out(game);
-	if (game->i.max_o.symbol_index == 42)
-		mlx_put_image_to_window(game->mlx, game->mlxw, game->i.max_o.reference, game->i.max_o.pixel_loc.x, game->i.max_o.pixel_loc.y);
 	//exit_game();
 }
